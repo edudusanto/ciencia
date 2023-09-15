3915e0e182f1c1c2222d1e93b290a2dd52046af1
edudusanto <142506588+edudusanto@users.noreply.github.com>
2023-09-15 12:20:58 -0300

    Add files via upload

---
+++ cienciaconputaçao/teste.py
@@ -0,0 +1,9 @@
+#Variáveis
+ #print('Ola mundo' )
+ #idade = 18
+ ##velocidade_Maxima = 240
+ #print(velocidade_Maxima)
+#frase_De_boas_vindas = "olá, seja bem-vindo(a)!" #String
+#print(frase_De_boas_vindas)
+posso_Entrar_Em_Contato = False  # Boleanos
+print(posso_Entrar_Em_Contato)
